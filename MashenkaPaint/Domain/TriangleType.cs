fac9c761232557d82899d319992a7ab5a6e60640
MariiaRev <maria.revenko2209@gmail.com>
2021-01-18 21:48:15 +0200

    Added integer values for values in enums TriangleType and LineType

--- MashenkaPaint/Domain/TriangleType.cs
+++ MashenkaPaint/Domain/TriangleType.cs
@@ -2,9 +2,9 @@
 {
     public enum TriangleType
     {
-        Type1,
-        Type2,
-        Type3,
-        Type4,
+        Type1 = 1,
+        Type2 = 2,
+        Type3 = 3,
+        Type4 = 4,
     }
 }
